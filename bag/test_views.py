eb1a84306dbcddc525f3e9166026e1e9c31b87f8
8Stringking <tomcondemned@hotmail.co.uk>
2022-07-29 00:53:28 +0000

    created test_views.py in home app, and ran 1st successful test testing weather the correct template was being rendered in the index view

--- bag/test_views.py
+++ bag/test_views.py
@@ -13,4 +13,3 @@ class TestViews(TestCase):
         response = self.client.get('/bag/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bag/bag.html')
-
