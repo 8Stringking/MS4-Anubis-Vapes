79ec1191a2532cf014eac4591c959a5ddb7fa89a
8Stringking <tomcondemned@hotmail.co.uk>
2022-08-04 01:49:18 +0000

    written the models for the wishlist app

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,30 @@
+''' defines wishlist class and related methods '''
 from django.db import models
+from profiles.models import UserProfile
+from products.models import Product
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    '''
+    saves products to a specific profile
+    '''
+    name = models.CharField(default="My Wishlist",
+                            max_length=250, null=True, blank=True)
+    profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class WishListItem(models.Model):
+    '''
+    defines fields for WishListItem class
+    '''
+    wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE,
+                                 related_name='wishitems')
+
+    product = models.ForeignKey(Product, null=False, blank=False,
+                                on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.wishlist)
