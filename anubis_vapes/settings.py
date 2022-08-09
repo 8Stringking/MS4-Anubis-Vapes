7f1a77361fbf3fd58292dd991526f349aac68338
8Stringking <tomcondemned@hotmail.co.uk>
2022-08-09 01:37:33 +0000

    2nd attempt to deploy to heroku completed stage 2

--- anubis_vapes/settings.py
+++ anubis_vapes/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['anubis-vapes.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -113,14 +114,20 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'anubis_vapes.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
